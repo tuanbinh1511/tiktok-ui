8638328a2c33a709c7646a228efca95d8d0e9f07
unknown <tuanbinh.hehe@gmail.com>
2022-12-17 22:38:30 +0700

    Header2/UI

---
+++ src/components/Layout/DefaultLayout/AccountItem/index.js
@@ -0,0 +1,23 @@
+import classNames from "classnames/bind";
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+
+
+const cx = classNames.bind(styles)
+function AccountItem() {
+    return ( 
+        <div className={cx('wapper')}>
+            <img className={cx('avartar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/05aa222706c452c8ad0ea7d2e719a745~c5_100x100.jpeg?x-expires=1671462000&x-signature=1Wvs6eHEC06lFEIFOnpIio111TI%3D" alt="Hoa" />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
+                </p>
+                <span className={cx('username')}>Nguyen Van A</span>
+            </div>
+        </div>
+     );
+}
+
+export default AccountItem;
